75296390a89c9ddb2949c9edfd610546da567f73
Vasil Genov <v1@Vasils-MacBook-Pro.local>
2024-10-19 13:44:23 +0300

    setup fin

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,57 @@
+import {
+    WebGLRenderer,
+    Scene,
+    OrthographicCamera,
+    ShaderMaterial,
+    PlaneGeometry,
+    Mesh,
+} from "three"
+
+import vertexShader from "./shaders/vertex-shader.glsl"
+import fragmentShader from "./shaders/fragment-shader.glsl"
+
 import "./style.css"
-import typescriptLogo from "./typescript.svg"
-import viteLogo from "/vite.svg"
-import { setupCounter } from "./counter.ts"
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+function onWindowResize(three: WebGLRenderer) {
+    three.setSize(window.innerWidth, window.innerHeight)
+}
 
-setupCounter(document.querySelector<HTMLButtonElement>("#counter")!)
+class SimonDevGLSLCourse {
+    #three: WebGLRenderer
+    #scene: Scene
+    #camera: OrthographicCamera
+
+    constructor() {
+        this.#three = new WebGLRenderer()
+        this.#scene = new Scene()
+        this.#camera = new OrthographicCamera(0, 1, 1, 0, 0.1, 1000)
+
+        document.body.appendChild(this.#three.domElement)
+        this.#raf()
+        this.#render()
+    }
+
+    #raf() {
+        requestAnimationFrame(() => {
+            this.#three.render(this.#scene, this.#camera)
+            this.#raf()
+        })
+    }
+
+    #render() {
+        onWindowResize(this.#three)
+        window.addEventListener("resize", () => onWindowResize(this.#three))
+
+        this.#camera.position.set(0, 0, 1)
+
+        const plane = new Mesh(
+            new PlaneGeometry(1, 1),
+            new ShaderMaterial({ uniforms: {}, vertexShader, fragmentShader }),
+        )
+
+        plane.position.set(0.5, 0.5, 0)
+        this.#scene.add(plane)
+    }
+}
+
+new SimonDevGLSLCourse()
