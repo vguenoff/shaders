a8be2d9eae6d4f5f3311acb2600d9e9ed37bdbbd
Vasil Genov <vguenoff@gmail.com>
2024-10-20 20:17:59 +0300

    Introduction and background finish

--- src/courses/SimonDevGLSL/shaders/s01-intro/fragment-shader.glsl
+++ src/courses/SimonDevGLSL/shaders/s01-intro/fragment-shader.glsl
@@ -3,6 +3,13 @@
 //     return vec4(1.0);
 // }
 
+// Type Qualifiers - changing the behaviour of a variable a bit
+// Sometimes global variables will have another qualifier before the "type"
+// 3 seperate ways to set "parameters" on shader programs
+// uniform vec4 example1;
+// attribute vec4 example2;
+// varying vec4 example3;
+
 // Bigges use of this is to return multiple values
 // by declaring multiple out parameters
 void getClolorUsingOut(in vec4 color, out vec4 final) {
