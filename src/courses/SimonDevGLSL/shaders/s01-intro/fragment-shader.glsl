0c58296b1303bddcc2978f2a5632aefd28171645
Vasil Genov <vguenoff@gmail.com>
2024-10-20 18:41:00 +0300

    Simple shader

--- src/courses/SimonDevGLSL/shaders/s01-intro/fragment-shader.glsl
+++ src/courses/SimonDevGLSL/shaders/s01-intro/fragment-shader.glsl
@@ -1,4 +1,18 @@
 
+// vec4 getColor() {
+//     return vec4(1.0);
+// }
+
+// Bigges use of this is to return multiple values
+// by declaring multiple out parameters
+void getClolorUsingOut(in vec4 color, out vec4 final) {
+    final = color * vec4(0.5);
+}
+
 void main() {
-    gl_FragColor = vec4(vec3(0.1, 0.1, 0.1), 1.0);
+    vec4 final;
+    // assigns the result fo final using out
+    getClolorUsingOut(vec4(0.0, 0.0, 1.0, 1.0), final);
+
+    gl_FragColor = final;
 }
